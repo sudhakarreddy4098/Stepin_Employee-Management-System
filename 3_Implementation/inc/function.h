0b935b746fa6555b6274756f61055b1050723c74
Mudugulla Venkata Sudhakar Reddy <85819741+sudhakarreddy4098@users.noreply.github.com>
2021-09-05 17:49:50 +0530

    Add files via upload

--- 3_Implementation/inc/function.h
+++ 3_Implementation/inc/function.h
@@ -1,1 +1,18 @@
+#ifndef FUNCTION_H_INCLUDED
+#define FUNCTION_H_INCLUDED
 
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+void add(FILE * fp); //to add to list
+void modify(FILE * fp);//to modify a record
+void displayList(FILE * fp);//display whole list
+void searchRecord(FILE *fp);//find a particular record
+void printChar(char ch,int n);//printing a character ch n times
+void printHead();
+
+
+#endif
